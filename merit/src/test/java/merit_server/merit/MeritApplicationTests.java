4b1438c016bc6d7bbf9895c1c65e0fede1e52582
seung-je <csz0209@gmail.com>
2024-03-03 03:39:30 +0900

    init

---
+++ merit/src/test/java/merit_server/merit/MeritApplicationTests.java
@@ -0,0 +1,13 @@
+package merit_server.merit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
